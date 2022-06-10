377b56d794e76695f8ff7ed5c95c6acb343bfc7e
Steven Fabian <SFabian@student.fullsail.edu>
2022-06-09 21:04:56 -0400

    process box/text/button

--- Mineweeper3.0/cMain.h
+++ Mineweeper3.0/cMain.h
@@ -11,4 +11,8 @@ public:
 	wxButton* m_btn1 = nullptr;
 	wxTextCtrl* m_txt1 = nullptr;
 	wxListBox* m_list1 = nullptr;
+
+	void OnButtonClicked(wxCommandEvent &evt);
+
+	wxDECLARE_EVENT_TABLE();
 };
